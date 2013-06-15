0829fb65dd45edce55ccc1f9fb8c6814c43daf10
Zhongwen Xu <zhongwen19901215@gmail.com>
2013-06-15 15:28:21 +0800

    Comments on ApplicationHelper.java

--- src/cn/edu/zju/cs/onlinepayment/personal/ApplicationHelper.java
+++ src/cn/edu/zju/cs/onlinepayment/personal/ApplicationHelper.java
@@ -1,22 +1,32 @@
 package cn.edu.zju.cs.onlinepayment.personal;
 import android.app.Application;
 
-public class ApplicationHelper extends Application {
+/*
+Application Helper 
+is used to store some Application Context such as session, login status
+All activities can access these data
+*/
 
+public class ApplicationHelper extends Application {
+	//Application context parameters
 	 private static boolean session_login = false;
 	 
+	 //modify the Application context parameters
 	 public void putSession(boolean bol) {
 		  session_login = bol;
 	 }
 	 
+	 //Make Login
 	 public void makeLogin(){
 		 session_login = true;
 	 }
 	 
+	 //Make Logout
 	 public void makeLogout(){
 		 session_login = false;
 	 }
 	 
+	 //return the status
 	 public boolean isLogin(){
 		 return session_login;
 	 }
