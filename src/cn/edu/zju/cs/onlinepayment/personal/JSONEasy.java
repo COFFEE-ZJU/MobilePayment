91dc44c8599a599dc5de164236f94ac31bb2b0f2
Zhongwen Xu <zhongwen19901215@gmail.com>
2013-06-15 15:58:55 +0800

    Comments on JSONEasy.java

--- src/cn/edu/zju/cs/onlinepayment/personal/JSONEasy.java
+++ src/cn/edu/zju/cs/onlinepayment/personal/JSONEasy.java
@@ -5,13 +5,21 @@ import java.util.Map.Entry;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+/*
+JSON Easy class, which extends JSONObject
+and for easy usage
 
+especially for HashMap
+*/
 public class JSONEasy extends JSONObject {
+	//JSON object in the class
 	JSONObject jsonObject;
 	public JSONEasy(HashMap<String, String> hashMap){
 		jsonObject = new JSONObject();
 		try{
+			//iterate all entries in the hashMap
 			for (Entry<String, String> entry : hashMap.entrySet()) {
+			    //put the key and value into the JSON object
 			    jsonObject.put(entry.getKey(), entry.getValue());
 			}
 		}catch (JSONException e){
@@ -20,9 +28,12 @@ public class JSONEasy extends JSONObject {
 	}
 	
 	public JSONEasy(String[] keys, String[] values){
+		//asser the length of keys array and the length of values array
 		assert(keys.length == values.length);
+		
 		jsonObject = new JSONObject();
 		try{
+			//put all keys and values into the object
 			for(int i = 0;i < keys.length; ++i){
 				jsonObject.put(keys[i],  values[i]);
 			}
